cb4762d2fa09f904abd35c203e6f180a06648d77
priyanka-tecnots <Priyanka.T@Tecnots.com>
2025-05-12 10:48:43 +0530

    <>|Update main.py|<Priyanka>

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from db_connection import engine
-import pandas as pd
+
 
 
 
 def main():
-    df = pd.read_sql("select * FROM sales_transactions",engine)
-    df['date'] =  pd.to_datetime(df['date'],errors='coerce')
-    print(df['date'])
+    # Code here
     
     
     
